acfeeab7b28580cc225bd84d658506ed8808d902
Aakarsh2207 <aakarsh22sharma007@gmail.com>
2025-04-10 01:06:43 +0530

    Backend Work Progress

---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowecase: true,
+    },
+    password:{
+        type: String,
+        required: [true, 'Password is required']
+    },
+    ava:{
+        type: String,
+        required: true,
+    }
+},{timestamps:true})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
